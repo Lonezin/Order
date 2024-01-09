a934226ec94f9f4559b934eb85ef97704d893a49
Vinicius Morais <vinidev.morais@gmail.com>
2024-01-09 12:38:38 -0300

    update order class

--- Entities/Order.cs
+++ Entities/Order.cs
@@ -6,15 +6,35 @@ namespace Course.Entities
     {
         public DateTime Date { get; set; } 
         public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
 
         public Order ()
         {   
         }
-        public Order(DateTime date, OrderStatus status)
+        public Order(DateTime date, OrderStatus status, Client client)
         {
             Date = date;
             Status = status;
+            Client = client;
         }
-        
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+
     }
 }
